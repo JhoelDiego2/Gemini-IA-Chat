7620e88f9db0aea3561f72146a376e734a6f0650
JhoelDiego2 <jhoel.mita@sptech.school>
2025-05-31 16:09:41 -0300

    arrumando o erro no arquivo main js

---
+++ main.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+
+require("dotenv").config();
+
+
+
+// configurando o servidor express
+const app = express();
+const PORTA_SERVIDOR = process.env.PORTA;
+
+
+var bobiaRouter = require("./src/routes/bobia");
+
+
+// configurando o servidor para receber requisições JSON
+app.use(express.json());
+
+// configurando o servidor para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+// configurando CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    next();
+});
+
+app.use("/bobia", bobiaRouter);
+// inicializando o servidor
+app.listen(PORTA_SERVIDOR, () => {
+    console.info(
+        `
+        ######                ###    #    
+        #     #  ####  #####   #    # #   
+        #     # #    # #    #  #   #   #  
+        ######  #    # #####   #  #     # 
+        #     # #    # #    #  #  ####### 
+        #     # #    # #    #  #  #     # 
+        ######   ####  #####  ### #     # 
+        `
+    );
+    console.info(`A API BobIA iniciada, acesse http://localhost:${PORTA_SERVIDOR}`);
+});
